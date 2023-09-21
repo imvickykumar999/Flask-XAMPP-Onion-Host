cf60c07f02046e5f276ec0eee8c71ce0eb2af831
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-21 13:31:20 +0530

    flask hosted

--- Tutorial Files/tor_example.py
+++ Tutorial Files/tor_example.py
@@ -1,20 +1,25 @@
 # https://jordan-wright.com/blog/2014/10/06/creating-tor-hidden-services-with-python/
 
+# First open tor browser,
+# then run this file
+
 from stem.control import Controller
 from flask import Flask
 
 if __name__ == "__main__":
+    app = Flask("__name__")
 
-    app = Flask("example")
     port = 5000
     host = "127.0.0.1"
-    hidden_svc_dir = r"C:\Users\Vicky\Desktop\Repository\Host-Onion\Tor Browser\tor_example"
+    hidden_svc_dir = "C:/Users/Vicky/Desktop/Repository/Host-Onion/Tor Browser/tor_example"
 
     @app.route('/')
     def index():
         return "<h1>Tor works!</h1>"
+    
     print (" * Getting controller")
     controller = Controller.from_port(address="127.0.0.1", port=9151)
+
     try:
         controller.authenticate(password="")
         controller.set_options([
@@ -23,6 +28,7 @@ if __name__ == "__main__":
             ])
         svc_name = open(hidden_svc_dir + "/hostname", "r").read().strip()
         print (" * Created host: %s" % svc_name)
+        
     except Exception as e:
         print (e)
     app.run()
