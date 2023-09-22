57c0c27f2caa2397e73c306c4b99a84e1a73ca98
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-22 20:22:39 +0530

    VicksTor Library

--- Tutorial Files/flask_app.py
+++ Tutorial Files/flask_app.py
@@ -1,10 +1,7 @@
 
-# https://jordan-wright.com/blog/2014/10/06/creating-tor-hidden-services-with-python/
-
-# First open tor browser,
-# then run this file
-
 from flask import Flask
+import VicksTor
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,28 +9,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    from stem.control import Controller
-    
-    port = 5000
-    host = "127.0.0.1"
-    hidden_svc_dir = "C:/Users/Vicky/Desktop/Repository/Host-Onion/Tor Browser/HiddenService/flask_app"
-
-    print (" * Getting controller")
-    controller = Controller.from_port(address=host, port=9151)
-
-    try:
-        controller.authenticate(password="")
-        controller.set_options([
-            ("HiddenServiceDir", hidden_svc_dir),
-            ("HiddenServicePort", "80 %s:%s" % (host, str(port)))
-            ])
-        svc_name = open(hidden_svc_dir + "/hostname", "r").read().strip()
-        print (" * Created host: %s" % svc_name)
-
-    except Exception as e:
-        print (e)
-
-    app.run(
-        host="0.0.0.0", 
-        debug=True
-    )
+    app.run()
--- sample_flask/flask_app.py
+++ sample_flask/flask_app.py
@@ -109,28 +109,5 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    from stem.control import Controller
-    
-    port = 5000
-    host = "127.0.0.1"
-    hidden_svc_dir = "C:/Users/Vicky/Desktop/Repository/Host-Onion/Tor Browser/HiddenService/sample_flask"
-
-    print (" * Getting controller")
-    controller = Controller.from_port(address=host, port=9151)
-
-    try:
-        controller.authenticate(password="")
-        controller.set_options([
-            ("HiddenServiceDir", hidden_svc_dir),
-            ("HiddenServicePort", "80 %s:%s" % (host, str(port)))
-            ])
-        svc_name = open(hidden_svc_dir + "/hostname", "r").read().strip()
-        print (" * Created host: %s" % svc_name)
-
-    except Exception as e:
-        print (e)
-
-    app.run(
-        host="0.0.0.0", 
-        debug=True
-    )
+    import VicksTor
+    app.run(host="0.0.0.0")
