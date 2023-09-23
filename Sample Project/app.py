b8bb6181b8cb81da613b834cc59895e351414c50
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-23 20:52:08 +0530

    video

--- Sample Project/app.py
+++ Sample Project/app.py
@@ -1,20 +1,13 @@
 
-# https://pypi.org/project/VicksTor/
-from flask import Flask, render_template
-import os
-
 from HostTor import VicksTor
 import VicksTor
 
-# template_dir = os.path.abspath('./')
-app = Flask(
-        __name__, 
-        # template_folder=template_dir
-    )
+from flask import Flask, render_template
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run()
