bd1133def8541176a25a32ba227d7f9ff7879801
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-29 18:42:49 +0530

    live on tor

--- Projects/Red Room/app.py
+++ Projects/Red Room/app.py
@@ -1,13 +1,21 @@
 
 # from HostTor import VicksTor
 import VicksTor
-VicksTor.run_server('flask')
 
 from flask import Flask, render_template, Response
 import cv2
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)
+VicksTor.run_server('flask')
+
+username = 'imvickykumar999'
+password = 'imvickykumar999'
+ip = '192.168.0.101'
+port = '8080'
+
+camera = cv2.VideoCapture('http://{username}:{password}@{ip}:{port}/video')
+# camera = cv2.VideoCapture('http://{ip}:{port}/video') # no authentication
+# camera = cv2.VideoCapture(0) # laptop webcam
 
 def generate_frames():
     while True:
@@ -17,7 +25,7 @@ def generate_frames():
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg',frame)
+            ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
 
         yield(b'--frame\r\n'
