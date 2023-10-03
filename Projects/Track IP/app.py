f57585317fc247cd442fd75686ab5ff3d17daf62
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-03 20:25:58 +0530

    client ip address

--- Projects/Track IP/app.py
+++ Projects/Track IP/app.py
@@ -1,18 +1,24 @@
 
 # https://stackoverflow.com/a/3760309/11493297
 
-from flask import request, Flask
+import socket
+from flask import Flask
 # from HostTor import VicksTor
-# import VicksTor
+import VicksTor
 
-# VicksTor.run_server('flask')
+VicksTor.run_server('flask')
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def get_my_ip():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+
     return f'''
 <meta http-equiv="refresh" content="3; URL=/">
-{request.remote_addr}
+hostname : {hostname}
+<br>
+ip_address : {ip_address}
 ''', 200
 
 if __name__ == '__main__':
