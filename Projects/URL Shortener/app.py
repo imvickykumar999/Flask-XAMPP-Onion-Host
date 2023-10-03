e553091bf0fd4c35328b2ec6b11e75240292a525
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-03 21:06:27 +0530

    URL Shortener

---
+++ Projects/URL Shortener/app.py
@@ -0,0 +1,89 @@
+# URL Shortener using Flask
+# https://machinelearningprojects.net/url-shortener-using-flask/
+
+# from HostTor import VicksTor
+import VicksTor
+VicksTor.run_server('flask')
+
+import sqlite3
+from hashids import Hashids
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this should be a secret random string'
+
+connection = sqlite3.connect('database.db')
+c = connection.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS urls(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            original_url TEXT NOT NULL,
+            clicks INTEGER NOT NULL DEFAULT 0
+        )''')
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    conn = get_db_connection()
+
+    if request.method == 'POST':
+        url = request.form['url']
+
+        if not url:
+            flash('The URL is required!')
+            return redirect(url_for('index'))
+
+        url_data = conn.execute('INSERT INTO urls (original_url) VALUES (?)',(url,))
+        conn.commit()
+        conn.close()
+
+        url_id = url_data.lastrowid
+        hashid = hashids.encode(url_id)
+        short_url = request.host_url + hashid
+
+        return render_template('index.html', short_url=short_url)
+
+    return render_template('index.html')
+
+@app.route('/<id>')
+def url_redirect(id):
+    conn = get_db_connection()
+
+    original_id = hashids.decode(id)
+    if original_id:
+        original_id = original_id[0]
+        url_data = conn.execute('SELECT original_url, clicks FROM urls WHERE id = (?)', (original_id,)).fetchone()
+        original_url = url_data['original_url']
+        clicks = url_data['clicks']
+
+        conn.execute('UPDATE urls SET clicks = ? WHERE id = ?',(clicks+1, original_id))
+        conn.commit()
+        conn.close()
+        return redirect(original_url)
+    else:
+        flash('Invalid URL')
+        return redirect(url_for('index'))
+
+@app.route('/stats')
+def stats():
+    conn = get_db_connection()
+    db_urls = conn.execute('SELECT id, created, original_url, clicks FROM urls').fetchall()
+    conn.close()
+
+    urls = []
+    for url in db_urls:
+        url = dict(url)
+        url['short_url'] = request.host_url + hashids.encode(url['id'])
+        urls.append(url)
+
+    return render_template('stats.html', urls=urls)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
