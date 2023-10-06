774d4f23afdf3081f37e535d488ebf3df59026ed
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-06 11:55:23 +0530

    web hosting on esp32

---
+++ ESP32/hosting/hosting.ino
@@ -0,0 +1,52 @@
+// ESP32 Web Server - STA Mode  
+
+#include <WiFi.h>
+#include <WebServer.h>
+
+// SSID & Password
+const char* ssid = "*******";  // Enter your SSID here
+const char* password = "**********";  //Enter your Password here
+
+WebServer server(80);  // Object of WebServer(HTTP port, 80 is defult)
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Try Connecting to ");
+  Serial.println(ssid);
+
+  // Connect to your wi-fi modem
+  WiFi.begin(ssid, password);
+
+  // Check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected successfully");
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
+
+  server.on("/", handle_root);
+
+  server.begin();
+  Serial.println("HTTP server started");
+  delay(100); 
+}
+
+void loop() {
+  server.handleClient();
+}
+
+// HTML & CSS contents which display on web server
+String HTML = "<!DOCTYPE html>\
+<html>\
+<body>\
+<h1>My First Web Server with ESP32 - Station Mode &#128522;</h1>\
+</body>\
+</html>";
+
+// Handle root url (/)
+void handle_root() {
+  server.send(200, "text/html", HTML);
+}
