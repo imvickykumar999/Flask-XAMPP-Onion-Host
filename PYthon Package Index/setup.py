2f1c5dfe5236caf91ddaefdad8bac7446b9687a4
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-13 20:56:52 +0530

    .whl and .tar.gz installtion
    Installation
        \dist> `pip install VicksTor-0.1.1-py3-none-any.whl`
        or,    `pip install VicksTor-0.1.2.tar.gz`
        or,    `pip install VicksTor`

--- PYthon Package Index/setup.py
+++ PYthon Package Index/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name = 'VicksTor', # while installing pacakge
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'Hosting Flask on Tor Browser.',
     long_description = open('Readme.txt').read(),
     url = 'https://github.com/imvickykumar999/Flask-XAMPP-Onion-Host',
     author = 'Vicky Kumar',
-    keywords = ['Hosting', 'Flask', 'XAMPP', 'Tor Browser'],
+    keywords = ['Hosting', 'Flask', 'XAMPP', 'Tor Browser', 'Onion'],
     license = 'MIT',
     packages = ['HostTor'], # while importing package
 )
