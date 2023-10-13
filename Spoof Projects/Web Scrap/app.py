7cd12f056d627d1177b2549f3f6e3d7bbc831eb2
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-13 15:48:52 +0530

    web scrapping

---
+++ Spoof Projects/Web Scrap/app.py
@@ -0,0 +1,21 @@
+
+# from HostTor import VicksTor
+import VicksTor as vix
+vix.run_server('flask')
+
+from flask import Flask
+import requests
+
+site = input('\nEnter link : ')
+if site == '':
+    site = 'https://github.com/imvickykumar999'
+
+req = requests.get(site)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return req.text
+
+if __name__ == '__main__':
+    app.run(debug=False)
