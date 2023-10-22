1837869a116b6f4df143b440e8c0eb7712bc13ac
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-22 20:38:04 +0530

    formatting

--- Spoof Projects/Index of/app.py
+++ Spoof Projects/Index of/app.py
@@ -1,12 +1,8 @@
 
 # # from HostTor import VicksTor
 # import VicksTor as vix
+
 # vix.run_server('xampp')
-
-# --------------------------------
-
-# # from HostTor import VicksTor
-# import VicksTor as vix
 # vix.run_server('flask')
 
 from flask import (
@@ -16,6 +12,8 @@ from flask import (
 )
 
 import os
+from datetime import datetime
+
 app = Flask(__name__, template_folder='./')
 path = 'Reels'
 
@@ -23,18 +21,24 @@ path = 'Reels'
 def send_file(filename):
     return send_from_directory(f'./{path}', filename)
 
-@app.route(f'/index_files/<path:filename>')  
-def index_files(filename):
-    return send_from_directory(f'./index_files', filename)
-
 @app.route('/')
 def hello_world():
+    mod = []
     files = os.listdir(f'./{path}')
+
+    for i in files:
+        t = os.path.getmtime(f'./{path}/{i}')
+        t = datetime.fromtimestamp(t).strftime('%d-%m-%Y %H:%M:%S')
+
+        s = os.path.getsize(f'./{path}/{i}') / 1048576
+        s = round(s, 2)
+        mod.append((i, t, s))
+
     return render_template(
         'index.html', 
-        files=files, 
         path=path,
+        files=mod, 
     )
 
 if __name__ == '__main__':
-    app.run(debug = 1)
+    app.run(debug = False)
